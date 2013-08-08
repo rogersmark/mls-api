56c3e4de828835405dbb9cbd942f1c9956124900
Mark Rogers <f4nt@f4ntasmic.com>
2013-08-07 20:10:41 -0500

    more api tweaks

--- mls_api/api.py
+++ mls_api/api.py
@@ -1,33 +1,58 @@
-from rest_framework import viewsets, routers
+from rest_framework import viewsets, routers, serializers, fields
 
 from mls_api import models
 
 
-class CompetitionViewSet(viewsets.ModelViewSet):
-    model = models.Competition
+class CompetitionSerializer(serializers.ModelSerializer):
+
+    standings = fields.Field(source='standings')
+
+    class Meta:
+        model = models.Competition
 
 
-class TeamViewSet(viewsets.ModelViewSet):
+class CompetitionViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.Competition.objects.all()
+    serializer_class = CompetitionSerializer
+
+
+class TeamViewSet(viewsets.ReadOnlyModelViewSet):
     model = models.Team
+    filter_fields = ('name', 'slug',)
 
 
-class GameTeamViewSet(viewsets.ModelViewSet):
+class GameTeamViewSet(viewsets.ReadOnlyModelViewSet):
     model = models.GameTeam
 
 
-class StatSetViewSet(viewsets.ModelViewSet):
+class StatSetViewSet(viewsets.ReadOnlyModelViewSet):
     model = models.StatSet
 
 
-class GameViewSet(viewsets.ModelViewSet):
-    model = models.Game
+class GameSerializer(serializers.HyperlinkedModelSerializer):
+
+    statset_set = serializers.HyperlinkedRelatedField(
+        view_name='statset-detail',
+        lookup_field='pk',
+        many=True,
+        read_only=True,
+    )
+
+    class Meta:
+        model = models.Game
 
 
-class PlayerViewSet(viewsets.ModelViewSet):
+class GameViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = models.Game.objects.all()
+    serializer_class = GameSerializer
+
+
+class PlayerViewSet(viewsets.ReadOnlyModelViewSet):
     model = models.Player
+    filter_fields = ('first_name', 'last_name', 'team__name')
 
 
-class GamePlayerViewSet(viewsets.ModelViewSet):
+class GamePlayerViewSet(viewsets.ReadOnlyModelViewSet):
     model = models.GamePlayer
 
 
